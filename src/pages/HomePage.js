6b3fdbe14315978112f17c409fb79f2e4e357d75
UrieLBadilloG <ubadillog1600@alumno.ipn.mx>
2022-11-20 00:28:34 -0600

    Ajustando pantalla principal por componentes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,11 @@ import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import { ScrollbarWrapper } from "../components/scroll";
+import DeliveryPage from "./DeliveryPage";
+import ServicesPage from "./ServicesPage";
+import AutonomusPage from "./AutonomusPage";
+import BiologyPage from "./BiologyPage";
+import TestPage from "./TestPage";
 
 const HomePage = () => {
     const [open, setOpen] = useState(false);
@@ -18,6 +23,7 @@ const HomePage = () => {
     const vista1 = () => {
         setOpen(!open);
         if (!open) {
+            //presionado
             document.getElementById("item1").style.display = "none";
             document.getElementById("item2").style.gridColumn = "1/20";
         } else {
@@ -199,28 +205,7 @@ const HomePage = () => {
                 }}
             >
                 <ScrollbarWrapper>
-                    1. Un texto es una composición de signos codificados en un sistema de
-                    escritura que forma una unidad de sentido. También es una composición
-                    de caracteres imprimibles (con grafema) generados por un algoritmo de
-                    cifrado que, aunque no tienen sentido para cualquier persona, sí puede
-                    ser descifrado por su destinatario original. En otras palabras, un
-                    texto es un entramado de signos con una intención comunicativa que
-                    adquiere sentido en determinado contexto. Las ideas que comunica un
-                    texto están contenidas en lo que se suele denominar
-                    «macroproposiciones», unidades estructurales de nivel superior o
-                    global, que otorgan coherencia al texto constituyendo su hilo central,
-                    el esqueleto estructural que cohesiona elementos lingüísticos formales
-                    de alto nivel, como los títulos y subtítulos, la secuencia de
-                    párrafos, etc. En contraste, las «microproposiciones» son los
-                    elementos coadyuvantes de la cohesión de un texto, pero a nivel más
-                    particular o local. Esta distinción fue realizada por Teun van Dijk en
-                    1980.1 El nivel microestructural o local está asociado con el concepto
-                    de cohesión. Se refiere a uno de los fenómenos propios de la
-                    coherencia, el de las relaciones particulares y locales que se dan
-                    entre elementos lingüísticos, tanto los que remiten unos a otros como
-                    los que tienen la función de conectar y organizar. También es un
-                    conjunto de oraciones agrupadas en párrafos que habla de un tema
-                    determinado.
+                    <DeliveryPage />
                 </ScrollbarWrapper>
             </CardContent>
             <CardContent
@@ -235,28 +220,8 @@ const HomePage = () => {
                 }}
             >
                 <ScrollbarWrapper>
-                    2. Un texto es una composición de signos codificados en un sistema de
-                    escritura que forma una unidad de sentido. También es una composición
-                    de caracteres imprimibles (con grafema) generados por un algoritmo de
-                    cifrado que, aunque no tienen sentido para cualquier persona, sí puede
-                    ser descifrado por su destinatario original. En otras palabras, un
-                    texto es un entramado de signos con una intención comunicativa que
-                    adquiere sentido en determinado contexto. Las ideas que comunica un
-                    texto están contenidas en lo que se suele denominar
-                    «macroproposiciones», unidades estructurales de nivel superior o
-                    global, que otorgan coherencia al texto constituyendo su hilo central,
-                    el esqueleto estructural que cohesiona elementos lingüísticos formales
-                    de alto nivel, como los títulos y subtítulos, la secuencia de
-                    párrafos, etc. En contraste, las «microproposiciones» son los
-                    elementos coadyuvantes de la cohesión de un texto, pero a nivel más
-                    particular o local. Esta distinción fue realizada por Teun van Dijk en
-                    1980.1 El nivel microestructural o local está asociado con el concepto
-                    de cohesión. Se refiere a uno de los fenómenos propios de la
-                    coherencia, el de las relaciones particulares y locales que se dan
-                    entre elementos lingüísticos, tanto los que remiten unos a otros como
-                    los que tienen la función de conectar y organizar. También es un
-                    conjunto de oraciones agrupadas en párrafos que habla de un tema
-                    determinado.
+                    <DeliveryPage />
+
                 </ScrollbarWrapper>
             </CardContent>
             <CardContent
@@ -271,28 +236,8 @@ const HomePage = () => {
                 }}
             >
                 <ScrollbarWrapper>
-                    3. Un texto es una composición de signos codificados en un sistema de
-                    escritura que forma una unidad de sentido. También es una composición
-                    de caracteres imprimibles (con grafema) generados por un algoritmo de
-                    cifrado que, aunque no tienen sentido para cualquier persona, sí puede
-                    ser descifrado por su destinatario original. En otras palabras, un
-                    texto es un entramado de signos con una intención comunicativa que
-                    adquiere sentido en determinado contexto. Las ideas que comunica un
-                    texto están contenidas en lo que se suele denominar
-                    «macroproposiciones», unidades estructurales de nivel superior o
-                    global, que otorgan coherencia al texto constituyendo su hilo central,
-                    el esqueleto estructural que cohesiona elementos lingüísticos formales
-                    de alto nivel, como los títulos y subtítulos, la secuencia de
-                    párrafos, etc. En contraste, las «microproposiciones» son los
-                    elementos coadyuvantes de la cohesión de un texto, pero a nivel más
-                    particular o local. Esta distinción fue realizada por Teun van Dijk en
-                    1980.1 El nivel microestructural o local está asociado con el concepto
-                    de cohesión. Se refiere a uno de los fenómenos propios de la
-                    coherencia, el de las relaciones particulares y locales que se dan
-                    entre elementos lingüísticos, tanto los que remiten unos a otros como
-                    los que tienen la función de conectar y organizar. También es un
-                    conjunto de oraciones agrupadas en párrafos que habla de un tema
-                    determinado.
+                    <AutonomusPage />
+
                 </ScrollbarWrapper>
             </CardContent>
             <CardContent
@@ -307,28 +252,7 @@ const HomePage = () => {
                 }}
             >
                 <ScrollbarWrapper>
-                    4. Un texto es una composición de signos codificados en un sistema de
-                    escritura que forma una unidad de sentido. También es una composición
-                    de caracteres imprimibles (con grafema) generados por un algoritmo de
-                    cifrado que, aunque no tienen sentido para cualquier persona, sí puede
-                    ser descifrado por su destinatario original. En otras palabras, un
-                    texto es un entramado de signos con una intención comunicativa que
-                    adquiere sentido en determinado contexto. Las ideas que comunica un
-                    texto están contenidas en lo que se suele denominar
-                    «macroproposiciones», unidades estructurales de nivel superior o
-                    global, que otorgan coherencia al texto constituyendo su hilo central,
-                    el esqueleto estructural que cohesiona elementos lingüísticos formales
-                    de alto nivel, como los títulos y subtítulos, la secuencia de
-                    párrafos, etc. En contraste, las «microproposiciones» son los
-                    elementos coadyuvantes de la cohesión de un texto, pero a nivel más
-                    particular o local. Esta distinción fue realizada por Teun van Dijk en
-                    1980.1 El nivel microestructural o local está asociado con el concepto
-                    de cohesión. Se refiere a uno de los fenómenos propios de la
-                    coherencia, el de las relaciones particulares y locales que se dan
-                    entre elementos lingüísticos, tanto los que remiten unos a otros como
-                    los que tienen la función de conectar y organizar. También es un
-                    conjunto de oraciones agrupadas en párrafos que habla de un tema
-                    determinado.
+                    <ServicesPage />
                 </ScrollbarWrapper>
             </CardContent>
             <CardContent
@@ -343,28 +267,7 @@ const HomePage = () => {
                 }}
             >
                 <ScrollbarWrapper>
-                    5. Un texto es una composición de signos codificados en un sistema de
-                    escritura que forma una unidad de sentido. También es una composición
-                    de caracteres imprimibles (con grafema) generados por un algoritmo de
-                    cifrado que, aunque no tienen sentido para cualquier persona, sí puede
-                    ser descifrado por su destinatario original. En otras palabras, un
-                    texto es un entramado de signos con una intención comunicativa que
-                    adquiere sentido en determinado contexto. Las ideas que comunica un
-                    texto están contenidas en lo que se suele denominar
-                    «macroproposiciones», unidades estructurales de nivel superior o
-                    global, que otorgan coherencia al texto constituyendo su hilo central,
-                    el esqueleto estructural que cohesiona elementos lingüísticos formales
-                    de alto nivel, como los títulos y subtítulos, la secuencia de
-                    párrafos, etc. En contraste, las «microproposiciones» son los
-                    elementos coadyuvantes de la cohesión de un texto, pero a nivel más
-                    particular o local. Esta distinción fue realizada por Teun van Dijk en
-                    1980.1 El nivel microestructural o local está asociado con el concepto
-                    de cohesión. Se refiere a uno de los fenómenos propios de la
-                    coherencia, el de las relaciones particulares y locales que se dan
-                    entre elementos lingüísticos, tanto los que remiten unos a otros como
-                    los que tienen la función de conectar y organizar. También es un
-                    conjunto de oraciones agrupadas en párrafos que habla de un tema
-                    determinado.
+                    <BiologyPage />
                 </ScrollbarWrapper>
             </CardContent>
             <CardContent
@@ -379,28 +282,7 @@ const HomePage = () => {
                 }}
             >
                 <ScrollbarWrapper>
-                    6. Un texto es una composición de signos codificados en un sistema de
-                    escritura que forma una unidad de sentido. También es una composición
-                    de caracteres imprimibles (con grafema) generados por un algoritmo de
-                    cifrado que, aunque no tienen sentido para cualquier persona, sí puede
-                    ser descifrado por su destinatario original. En otras palabras, un
-                    texto es un entramado de signos con una intención comunicativa que
-                    adquiere sentido en determinado contexto. Las ideas que comunica un
-                    texto están contenidas en lo que se suele denominar
-                    «macroproposiciones», unidades estructurales de nivel superior o
-                    global, que otorgan coherencia al texto constituyendo su hilo central,
-                    el esqueleto estructural que cohesiona elementos lingüísticos formales
-                    de alto nivel, como los títulos y subtítulos, la secuencia de
-                    párrafos, etc. En contraste, las «microproposiciones» son los
-                    elementos coadyuvantes de la cohesión de un texto, pero a nivel más
-                    particular o local. Esta distinción fue realizada por Teun van Dijk en
-                    1980.1 El nivel microestructural o local está asociado con el concepto
-                    de cohesión. Se refiere a uno de los fenómenos propios de la
-                    coherencia, el de las relaciones particulares y locales que se dan
-                    entre elementos lingüísticos, tanto los que remiten unos a otros como
-                    los que tienen la función de conectar y organizar. También es un
-                    conjunto de oraciones agrupadas en párrafos que habla de un tema
-                    determinado.
+                    <TestPage />
                 </ScrollbarWrapper>
             </CardContent>
         </Card>
